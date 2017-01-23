462d1e3253c445822cfd62c58415fc827670c4a6
Jacky Leung <jacky@fishpond.co.nz>
2017-01-23 18:24:49 +1300

    Fix not install properly

--- manifests/install.pp
+++ manifests/install.pp
@@ -39,9 +39,11 @@ class codedeploy::install {
         owner   => 'root',
         group   => 'root',
         mode    => '0740',
+
       }
 
-      File<| $title == "${::staging::path}/codedeploy/install/mode" |> ~> Exec['install_codedeploy_agent']
+      Class['staging'] -> Class['codedeploy::install']
+      File<| $title == "${::staging::path}/codedeploy/install" |> ~> Exec['install_codedeploy_agent']
 
       exec { 'install_codedeploy_agent':
         command     => "${::staging::path}/codedeploy/install auto",
